882d8ca2921f9b2d94d83f9b33d90aadc598ab49
milanmithal <milan.mithal@hkdigitalonline.com>
2015-01-23 15:05:13 +0530

    Comman File

---
+++ common.php
@@ -0,0 +1,9 @@
+<?
+   $dbhost = "localhost";
+	$dbuser = "portalhk_akasva";
+	$dbpass = "akasva123";
+	$dbname = "portalhk_akasva";
+	
+	$link1 = mysql_connect($dbhost,$dbuser,$dbpass) or mysql_error("Not able to connect with MySql Server");
+	mysql_select_db($dbname,$link1) or mysql_error("Not able to connect with Database");
+?>
